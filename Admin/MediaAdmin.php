909a336d32b85298c9c4859a177fb1bcbd90d457
Michał Jędraszczyk <michal.jedraszczyk@Mac-mini-Wykop.local>
2016-11-18 11:59:53 +0100

    fix media

--- Admin/MediaAdmin.php
+++ Admin/MediaAdmin.php
@@ -8,32 +8,4 @@ use Sonata\MediaBundle\Form\DataTransformer\ProviderDataTransformer;
 
 class MediaAdmin extends Admin
 {
-    /**
-     * {@inheritdoc}
-     */
-    protected function configureFormFields( FormMapper $formMapper ) {
-        $media = $this->getSubject();
-
-        if ( ! $media ) {
-            $media = $this->getNewInstance();
-        }
-
-        if ( ! $media || ! $media->getProviderName() ) {
-            return;
-        }
-
-        $formMapper->add( 'providerName', 'hidden' );
-
-        $formMapper->getFormBuilder()->addModelTransformer( new ProviderDataTransformer( $this->pool, $this->getClass() ), true );
-
-        $provider = $this->pool->getProvider( $media->getProviderName() );
-
-        if ( $media->getId() ) {
-            $provider->buildEditForm( $formMapper );
-        } else {
-            $provider->buildCreateForm( $formMapper );
-        }
-
-    }
-
 }

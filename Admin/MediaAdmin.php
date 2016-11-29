2fc8eaa076fda26f5b0a4dc31a0b161d26af5a53
Michał Jędraszczyk <michal.jedraszczyk@appverk.com>
2016-11-29 12:57:47 +0100

    remove category from media admin

--- Admin/MediaAdmin.php
+++ Admin/MediaAdmin.php
@@ -3,9 +3,22 @@
 namespace Cube\CoreBundle\Admin;
 
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\MediaBundle\Admin\ORM\MediaAdmin as Admin;
-use Sonata\MediaBundle\Form\DataTransformer\ProviderDataTransformer;
+use Sonata\MediaBundle\Admin\ORM\MediaAdmin as BaseMediaAdmin;
+use Sonata\AdminBundle\Route\RouteCollection;
 
-class MediaAdmin extends Admin
+class MediaAdmin extends BaseMediaAdmin
 {
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection
+            ->remove('export');
+    }
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        parent::configureFormFields($formMapper);
+        $formMapper->remove('category');
+    }
 }
